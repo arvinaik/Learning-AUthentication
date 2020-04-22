0d7ac1689c98b45ef9c0c61c5c0ccd118f9347aa
Gary Peabody <aikarvin99@gmail.com>
2020-04-22 15:36:14 +0800

    Basic Authentication

---
+++ Basic database account/Secrets - Starting Code/app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+const loginSchema = new mongoose.Schema({ email: String, password: String });
+const userAccount = new mongoose.model("user login", loginSchema);
+
+express.static("public");
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.post("/register", function (req, res) {
+  let username = req.body.username;
+  let password = req.body.password;
+  const new_user = new userAccount({
+    email: username,
+    password: password,
+  });
+  new_user.save(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function (req, res) {
+  let username = req.body.username;
+  let password = req.body.password;
+  userAccount.findOne({ email: username }, function (err, accountFound) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (accountFound == null) {
+        res.render("errorLogin");
+      }
+
+      if (accountFound) {
+        if (accountFound.password === password) {
+          res.render("secrets");
+        } else {
+          res.render("errorLogin");
+        }
+      }
+    }
+  });
+});
+
+app.listen(2500, function () {
+  console.log("Server Up");
+});
